567c90834b493fd917d21486ee4f2fdb009ee5da
Marco Mazzei <marco.mazzei@cps.sp.gov.br>
2026-03-24 17:59:27 -0300

    Cálculo de salário efetuado de acordo com o tipo

---
+++ Empregado.java
@@ -0,0 +1,72 @@
+public class Empregado {
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario() {
+        if (tipo == 1) {
+            return salario;
+        } else if (tipo == 2) {
+            return salario + salario * comissao;
+        } else if (tipo == 3) {
+            return salario + bonus;
+        } else {
+            return 0;
+        }
+
+
+    }
+
+
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
