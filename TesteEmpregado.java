62a5e99eea4ad9cb953d4b3d70ea4bcdd811ccc1
Marco Mazzei <marco.mazzei@cps.sp.gov.br>
2026-03-26 16:15:23 -0300

    Adição do gitignore

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,13 +1,7 @@
 public class TesteEmpregado {
     public static void main(String[] args) {
-        String nome = "João";
-        String apelido = nome;
-        nome = "Maria";
-        System.out.println(apelido);
-        System.out.println(nome);
         Empregado empregado1 = new Empregado("Marco",58,1,5000,0,0);
-        Empregado empregado2 = empregado1;
-        empregado2.setNome("Ana");
+        Empregado empregado2 = new Empregado("Ana",30,2,3000,5,0);
         System.out.println(empregado1.getNome());
         System.out.println(empregado2.getNome());
     }
